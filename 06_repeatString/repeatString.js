690f3e6dbd823bcc9a16d4d4b5d503bc3426f2e6
AnaNijak <67751815+AnaNijak@users.noreply.github.com>
2025-07-09 12:22:06 +0200

    Complete 06_repeatString

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function (text, numberOfRepeats) {
+	if (numberOfRepeats < 0) return "ERROR";
+	if (text === "") return text;
 
+	let repeatedString = "";
+
+	let repeat = 0;
+	while (repeat < numberOfRepeats) {
+		repeatedString += text;
+		repeat++;
+	}
+
+	return repeatedString;
 };
 
 // Do not edit below this line
