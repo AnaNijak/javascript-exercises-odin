70c2d512e08ea3dd5f5a835b4b32285be6d8208c
AnaNijak <67751815+AnaNijak@users.noreply.github.com>
2025-07-09 12:39:28 +0200

    Complete 08_removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,5 @@
-const removeFromArray = function() {
+const removeFromArray = function (inputArray, ...itemsToBeRemoved) {
+	return inputArray.filter((element) => !itemsToBeRemoved.includes(element));
 };
 
 // Do not edit below this line
