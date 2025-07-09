13c297f813ea31c1dd9b5b33ad5d0aa573f801bf
AnaNijak <67751815+AnaNijak@users.noreply.github.com>
2025-07-09 13:09:11 +0200

    Complete 09_sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
+const sumAll = function (rangeStart, rangeEnd) {
+	if (!Number.isInteger(rangeStart) || !Number.isInteger(rangeEnd)) return "ERROR";
+	if (rangeStart < 0 || rangeEnd < 0) return "ERROR";
 
+	if (rangeStart > rangeEnd) [rangeStart, rangeEnd] = [rangeEnd, rangeStart];
+
+	let sum = 0;
+	for (let i = rangeStart; i <= rangeEnd; i++) sum += i;
+
+	return sum;
 };
 
 // Do not edit below this line
