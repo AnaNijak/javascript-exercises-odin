14cb42a851ee5c6e5fc2bf91f1ad599aa2abfab1
AnaNijak <67751815+AnaNijak@users.noreply.github.com>
2025-07-13 20:37:35 +0200

    Complete 16_findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+	let peopleWithAges = people.map((person) => {
+		let deathYear = person.yearOfDeath ? person.yearOfDeath : new Date().getFullYear();
 
+		return {
+			name: person.name,
+			age: deathYear - person.yearOfBirth,
+		};
+	});
+	let peopleWithAgesSorted = peopleWithAges.sort((a, b) => a.age - b.age);
+	let oldestPerson = peopleWithAgesSorted[people.length - 1];
+	return oldestPerson;
 };
 
 // Do not edit below this line
