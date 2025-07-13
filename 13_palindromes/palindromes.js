06278bb9636081d4e05c3d994967b10087b0d454
AnaNijak <67751815+AnaNijak@users.noreply.github.com>
2025-07-13 19:54:38 +0200

    Complete 13_palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (text) {
+	let cleanText = text
+		.toLowerCase()
+		.split("")
+		.filter((char) => /^[a-z0-9]*$/gi.test(char))
+		.join("");
 
+	let cleanReversedText = cleanText.split("").reverse().join("");
+	return cleanText.localeCompare(cleanReversedText) === 0;
 };
 
 // Do not edit below this line
