ea40606b40f321649b13c238bf5676f145a5e084
AnaNijak <67751815+AnaNijak@users.noreply.github.com>
2025-07-13 19:35:32 +0200

    Complete 12_calculator

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,33 +1,41 @@
-const add = function() {
-	
+const add = function (a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+	return array.reduce((sum, number) => {
+		return (sum += number);
+	}, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+	return array.reduce((product, number) => {
+		return product * number;
+	});
 };
 
-const power = function() {
-	
+const power = function (base, power) {
+	return base ** power;
 };
 
-const factorial = function() {
-	
+const factorial = function (number) {
+	let factorial = 1;
+	for (let i = 2; i <= number; i++) {
+		factorial = factorial * i;
+	}
+	return factorial;
 };
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+	add,
+	subtract,
+	sum,
+	multiply,
+	power,
+	factorial,
 };
